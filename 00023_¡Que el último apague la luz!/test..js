4dd312050ca44718bcd2c27ea4ec639eeefc9183
gustavo@mumuki.org <gustavo@mumuki.org>
2022-04-28 18:56:04 +0000

    Mumuki Export on 2022-04-28 18:56:04 +0000

--- 00023_¡Que el último apague la luz!/test..js
+++ 00023_¡Que el último apague la luz!/test..js
@@ -1,5 +1,16 @@
-describe("Descripción del grupo", function() {
-  it("Descripción del ejemplo", function() {
-    assert(true)
-  })
+it("si la mochila estaba abierta, al ejecutar usarCierre pasa a estar cerrada", function() {
+  usarCierre();
+  assert.equal(mochilaAbierta, false);
+})
+
+it("si la mochila estaba cerrada, al ejecutar usarCierre pasa a estar abierta", function() {
+  mochilaAbierta = false;
+  usarCierre();
+  assert.equal(mochilaAbierta, true);
+})
+
+it("si la mochila estaba abierta, al ejecutar usarCierre dos veces sigue estando abierta", function() {
+  usarCierre();
+  usarCierre();
+  assert.equal(mochilaAbierta, true);
 })
