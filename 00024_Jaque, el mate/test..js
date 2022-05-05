bc20cdd35ac551ca7920b73e1cda60b227fb85af
gustavo@mumuki.org <gustavo@mumuki.org>
2022-05-05 17:39:33 +0000

    Mumuki Export on 2022-05-05 17:39:33 +0000

--- 00024_Jaque, el mate/test..js
+++ 00024_Jaque, el mate/test..js
@@ -1,10 +1,10 @@
 it("cebar mate disminuye en 30 ml el agua del termo", () => { 
-  let aguaDelTermo = 1000;
+  aguaDelTermo = 1000;
   cebarMate()
   assert.equal(aguaDelTermo, 970)
 })
 it("cebar tres mates disminuye en 90 ml el agua del termo", () => { 
-  let aguaDelTermo = 1000;
+  aguaDelTermo = 1000;
   cebarMate()
   cebarMate()
   cebarMate()
@@ -12,13 +12,13 @@ it("cebar tres mates disminuye en 90 ml el agua del termo", () => {
 })
 
 it("cebar mate aumenta en 30 ml el agua del mate", () => {
-  let aguaDelMate = 0;
+  aguaDelMate = 0;
   cebarMate()
   assert.equal(aguaDelMate, 30)
 })
 
 it("tomar mate deja en 0 ml el agua del mate", () => {
-  let aguaDelMate = 20;
+  aguaDelMate = 20;
   tomarMate()
   assert.equal(aguaDelMate, 0)
 })
